5a3552c9f7dda5d744962212547d749ef4309140
Nahid51 <hasannahidnahid@gmail.com>
2021-12-26 22:16:35 +0600

    ssl implementation

--- index.js
+++ index.js
@@ -9,6 +9,64 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // data ke parse kore
 
+//sslcommerz init
+app.get('/init/:amount', (req, res) => {
+    const data = {
+        total_amount: req.params.amount,
+        currency: 'BDT',
+        tran_id: 'REF123',
+        success_url: 'https://frozen-falls-89510.herokuapp.com/success',
+        fail_url: 'https://frozen-falls-89510.herokuapp.com/fail',
+        cancel_url: 'https://frozen-falls-89510.herokuapp.com/cancel',
+        ipn_url: 'https://frozen-falls-89510.herokuapp.com/ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: 'cust@yahoo.com',
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+        multi_card_name: 'mastercard',
+        value_a: 'ref001_A',
+        value_b: 'ref002_B',
+        value_c: 'ref003_C',
+        value_d: 'ref004_D'
+    };
+    const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASS, false) //true for live default false for sandbox
+    sslcommer.init(data).then(data => {
+        //process the response that got from sslcommerz
+        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+        console.log(data);
+        res.redirect(data.GatewayPageURL);
+    });
+})
+app.post('/success', async (req, res) => {
+    console.log(req.body);
+    res.status(200).json(req.body);
+})
+app.post('/fail', async (req, res) => {
+    console.log(req.body);
+    res.status(400).json(req.body);
+})
+app.post('/cancel', async (req, res) => {
+    console.log(req.body);
+    res.status(200).json(req.body);
+})
+
 const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bsutc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -64,63 +122,7 @@ client.connect(err => {
         const result = await cursor.toArray();
         res.send(result);
     })
-    //sslcommerz init
-    app.get('/init/:amount', (req, res) => {
-        const data = {
-            total_amount: req.params.amount,
-            currency: 'BDT',
-            tran_id: 'REF123',
-            success_url: 'https://frozen-falls-89510.herokuapp.com/success',
-            fail_url: 'https://frozen-falls-89510.herokuapp.com/fail',
-            cancel_url: 'https://frozen-falls-89510.herokuapp.com/cancel',
-            ipn_url: 'https://frozen-falls-89510.herokuapp.com/ipn',
-            shipping_method: 'Courier',
-            product_name: 'Computer.',
-            product_category: 'Electronic',
-            product_profile: 'general',
-            cus_name: 'Customer Name',
-            cus_email: 'cust@yahoo.com',
-            cus_add1: 'Dhaka',
-            cus_add2: 'Dhaka',
-            cus_city: 'Dhaka',
-            cus_state: 'Dhaka',
-            cus_postcode: '1000',
-            cus_country: 'Bangladesh',
-            cus_phone: '01711111111',
-            cus_fax: '01711111111',
-            ship_name: 'Customer Name',
-            ship_add1: 'Dhaka',
-            ship_add2: 'Dhaka',
-            ship_city: 'Dhaka',
-            ship_state: 'Dhaka',
-            ship_postcode: 1000,
-            ship_country: 'Bangladesh',
-            multi_card_name: 'mastercard',
-            value_a: 'ref001_A',
-            value_b: 'ref002_B',
-            value_c: 'ref003_C',
-            value_d: 'ref004_D'
-        };
-        const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASS, false) //true for live default false for sandbox
-        sslcommer.init(data).then(data => {
-            //process the response that got from sslcommerz
-            //https://developer.sslcommerz.com/doc/v4/#returned-parameters
-            console.log(data);
-            res.redirect(data.GatewayPageURL);
-        });
-    })
-    app.post('/success', async (req, res) => {
-        console.log(req.body);
-        res.status(200).json(req.body);
-    })
-    app.post('/fail', async (req, res) => {
-        console.log(req.body);
-        res.status(400).json(req.body);
-    })
-    app.post('/cancel', async (req, res) => {
-        console.log(req.body);
-        res.status(200).json(req.body);
-    })
+
 
 
 
