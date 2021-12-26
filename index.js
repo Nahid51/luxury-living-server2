c7080f53e48c179abb3f8be801ff4da2525e63ff
Nahid51 <hasannahidnahid@gmail.com>
2021-12-27 01:21:25 +0600

    ssl implementation

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 const SSLCommerzPayment = require('sslcommerz');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(cors());
 app.use(express.json()); // data ke parse kore
@@ -65,7 +71,7 @@ client.connect(err => {
         res.send(result);
     })
     //sslcommerz init
-    app.get('/init', async (req, res) => {
+    app.use('/init', async (req, res) => {
         const data = {
             total_amount: '1010',
             currency: 'BDT',
